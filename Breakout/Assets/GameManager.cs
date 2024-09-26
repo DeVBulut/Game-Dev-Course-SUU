9eac8d039b73d1d2cd6c76441d6758f60d2e192b
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2024-09-25 20:39:52 -0600

    Game End State

--- Breakout/Assets/GameManager.cs
+++ Breakout/Assets/GameManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,30 +10,46 @@ public class GameManager : MonoBehaviour
     public GameObject BallPrefab;
     public float RespawnTimer;
     [SerializeField] [Range(0, 15)] private int _maxRespawns;
-    public int _respawnCount;
+    public int _respawnCount = 1;
+    [SerializeField]private TextMeshProUGUI _ballText;
+    [SerializeField] private GameObject _failedPanel;
+    [HideInInspector] public bool GameRunning;
 
     void Start()
     {
+        _failedPanel.SetActive(false);
+        GameRunning = true;
         Instance = this;
         GameObject ballObject = Instantiate(BallPrefab);
         ballObject.GetComponent<BallBehaviour>().ObjectVelocity = Vector3.down;
-        
+        UpdateBallText();
     }   
     public void BallOutOfBounds(GameObject ball)
     {
         Destroy(ball);
         _respawnCount += 1;
-
-        if (_respawnCount <= _maxRespawns)
+        if (_respawnCount < _maxRespawns)
         {
             StartCoroutine(WaitandRespawn());
+        }
+        else
+        {
+            UpdateBallText();
+            _failedPanel.SetActive(true);
+            GameRunning = false;
         }
     }
     private IEnumerator WaitandRespawn()
     {
+        UpdateBallText();
         GameObject ballObject = Instantiate(BallPrefab);
         ballObject.GetComponent<BallBehaviour>().ObjectVelocity = Vector3.zero;
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(RespawnTimer);
         ballObject.GetComponent<BallBehaviour>().ObjectVelocity = Vector3.down;
+    }
+
+    private void UpdateBallText()
+    {
+        _ballText.text = "Balls Remaining: " + (_maxRespawns - _respawnCount) + " / 3";
     }
 }
--- Breakout/Assets/PaddleController.cs
+++ Breakout/Assets/PaddleController.cs
@@ -21,6 +21,8 @@ public class CubeController : MonoBehaviour
     }
     void Update()
     {
+        if (!GameManager.Instance.GameRunning) return;
+
         if (Input.GetKey(KeyCode.LeftArrow) 
             && transform.position.x - StartScale.x / 2f > MinPosition.transform.position.x)
             transform.position += Vector3.left * Time.deltaTime * Speed;
